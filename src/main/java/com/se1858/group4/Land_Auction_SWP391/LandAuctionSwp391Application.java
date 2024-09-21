d44be5d1adec4f2636bffe58a2c12649417ed066
CoderFPT <139433806+CoderFPT@users.noreply.github.com>
2024-09-21 11:43:29 +0700

    Initial commit

---
+++ src/main/java/com/se1858/group4/Land_Auction_SWP391/LandAuctionSwp391Application.java
@@ -0,0 +1,13 @@
+package com.se1858.group4.Land_Auction_SWP391;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandAuctionSwp391Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandAuctionSwp391Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/se1858/group4/Land_Auction_SWP391/LandAuctionSwp391ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se1858.group4.Land_Auction_SWP391;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandAuctionSwp391ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
