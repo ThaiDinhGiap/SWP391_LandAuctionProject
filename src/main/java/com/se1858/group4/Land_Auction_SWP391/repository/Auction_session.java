425ce3735dc6956b8263c1be90a8ecccb34fae69
CoderFPT <139433806+CoderFPT@users.noreply.github.com>
2024-09-23 13:13:20 +0700

    Done Respository Package version 1.0

---
+++ src/main/java/com/se1858/group4/Land_Auction_SWP391/repository/Auction_session.java
@@ -0,0 +1,6 @@
+package com.se1858.group4.Land_Auction_SWP391.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Auction_session extends JpaRepository<Auction_session, Integer> {
+}
