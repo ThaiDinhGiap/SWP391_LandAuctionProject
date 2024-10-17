086538a0f6363ef904e23e7e4d9939d2b8e69101
Bui Minh Chien <chien181004@gmail.com>
2024-10-17 16:36:37 +0700

    lam giao dien asset cho customer

--- src/main/java/com/se1858/group4/Land_Auction_SWP391/controller/CustomerController.java
+++ src/main/java/com/se1858/group4/Land_Auction_SWP391/controller/CustomerController.java
@@ -1,10 +1,14 @@
 package com.se1858.group4.Land_Auction_SWP391.controller;
 
+import com.se1858.group4.Land_Auction_SWP391.entity.Asset;
 import com.se1858.group4.Land_Auction_SWP391.entity.News;
+import com.se1858.group4.Land_Auction_SWP391.entity.Tag;
 import com.se1858.group4.Land_Auction_SWP391.entity.TagForNews;
+import com.se1858.group4.Land_Auction_SWP391.service.AssetService;
 import com.se1858.group4.Land_Auction_SWP391.service.NewsService;
 import com.se1858.group4.Land_Auction_SWP391.service.TagForNewsService;
 import com.se1858.group4.Land_Auction_SWP391.service.TagService;
+import com.se1858.group4.Land_Auction_SWP391.utility.GetSrcInGoogleMapEmbededURLUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,14 +22,25 @@ import java.util.List;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-    private final TagService tagService;
     private NewsService newsService;
     private TagForNewsService tagForNewsService;
+    private AssetService assetService;
+    private TagService tagService;
     @Autowired
-    public CustomerController(NewsService newsService, TagService tagService, TagForNewsService tagForNewsService) {
+    public CustomerController(NewsService newsService, TagForNewsService tagForNewsService, AssetService assetService, TagService tagService) {
         this.newsService = newsService;
-        this.tagService = tagService;
         this.tagForNewsService = tagForNewsService;
+        this.assetService = assetService;
+        this.tagService = tagService;
+    }
+    @GetMapping("/get_all_asset")
+    public String getAllAsset(Model model) {
+        List<Asset> newsList=assetService.getAllAssetWithStatus("Waiting for Auction Scheduling");
+        model.addAttribute("listAsset",newsList);
+//        lay danh sach cac tag
+        List<Tag> tagList=tagService.getAllTag();
+        model.addAttribute("listTag",tagList);
+        return "customer/assetList";
     }
     @GetMapping("/get_all_news")
     public String getAllNews(Model model) {
@@ -39,7 +54,7 @@ public class CustomerController {
         model.addAttribute("listTag",tagList);
         return "customer/newsList";
     }
-    @GetMapping("/viewDetail")
+    @GetMapping("/viewNewsDetail")
     public String getNewsById(@RequestParam("newsId") int newsId, Model model) {
         News news=newsService.getNewsById(newsId);
         model.addAttribute("news",news);
@@ -50,5 +65,13 @@ public class CustomerController {
         List<TagForNews> tagList=tagForNewsService.getAllTagsForNews();
         model.addAttribute("listTag",tagList);
         return "customer/newsDetail";
+    }
+    @GetMapping("/viewAssetDetail")
+    public String getAssetById(@RequestParam("assetId") int assetId, Model model) {
+        Asset asset=assetService.getAssetById(assetId);
+        String embedUrl = GetSrcInGoogleMapEmbededURLUtil.extractSrcFromIframe(asset.getCoordinatesOnMap());
+        model.addAttribute("embedUrl", embedUrl);
+        model.addAttribute("asset",asset);
+        return "customer/assetDetail";
     }
 }
