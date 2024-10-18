3e4a30f6f3e5de2a0222855d27127aa9970d8960
Bui Minh Chien <chien181004@gmail.com>
2024-10-18 13:22:52 +0700

    sua man hinh news cho customer

--- src/main/java/com/se1858/group4/Land_Auction_SWP391/controller/CustomerController.java
+++ src/main/java/com/se1858/group4/Land_Auction_SWP391/controller/CustomerController.java
@@ -12,11 +12,13 @@ import com.se1858.group4.Land_Auction_SWP391.utility.GetSrcInGoogleMapEmbededURL
 import com.se1858.group4.Land_Auction_SWP391.service.CustomerService;
 import com.se1858.group4.Land_Auction_SWP391.service.ImageService;
 import com.se1858.group4.Land_Auction_SWP391.utility.FileUploadUtil;
+import com.se1858.group4.Land_Auction_SWP391.utility.QrCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -31,27 +33,16 @@ public class CustomerController {
     private AuctionService auctionService;
     private UserDetailsService userDetailsService;
     private AccountService accountService;
-
-    @Autowired
-    private AccountService accountService;
-    @Autowired
     private ImageService imageService;
     private FileUploadUtil uploadFile;
-    @Autowired
     private CustomerService customerService;
-
-    public CustomerController(UserDetailsService userDetailsService, AccountService accountService, CustomerService customerService, ImageService imageService, FileUploadUtil uploadFile) {
-        this.userDetailsService = userDetailsService;
-        this.accountService = accountService;
-        this.imageService = imageService;
-        this.uploadFile = uploadFile;
-        this.customerService = customerService;
-    }
-
+    private QrCode qrCode;
 
     public CustomerController(NewsService newsService, TagForNewsService tagForNewsService,
                               AssetService assetService, TagService tagService, AuctionService auctionService,
-                              UserDetailsService userDetailsService, AccountService accountService) {
+                              UserDetailsService userDetailsService, AccountService accountService,
+                              ImageService imageService, FileUploadUtil uploadFile,
+                              CustomerService customerService, QrCode qrCode) {
         this.newsService = newsService;
         this.tagForNewsService = tagForNewsService;
         this.assetService = assetService;
@@ -59,60 +50,70 @@ public class CustomerController {
         this.auctionService = auctionService;
         this.userDetailsService = userDetailsService;
         this.accountService = accountService;
+        this.imageService = imageService;
+        this.uploadFile = uploadFile;
+        this.customerService = customerService;
+        this.qrCode = qrCode;
     }
+
     @GetMapping("/get_all_asset")
     public String getAllAsset(Model model) {
-        List<Asset> newsList=assetService.getAllAssetWithStatus("Waiting for Auction Scheduling");
-        model.addAttribute("listAsset",newsList);
+        List<Asset> newsList = assetService.getAllAssetWithStatus("Waiting for Auction Scheduling");
+        model.addAttribute("listAsset", newsList);
 //        lay danh sach cac tag
-        List<Tag> tagList=tagService.getAllTag();
-        model.addAttribute("listTag",tagList);
+        List<Tag> tagList = tagService.getAllTag();
+        model.addAttribute("listTag", tagList);
         return "customer/assetList";
     }
+
     @GetMapping("/get_all_auction")
     public String getAllAuction(Model model) {
-        List<AuctionSession> auctionList=auctionService.getAllAutcion();
-        model.addAttribute("listAuction",auctionList);
+        List<AuctionSession> auctionList = auctionService.getAllAutcion();
+        model.addAttribute("listAuction", auctionList);
         return "customer/auctionList";
     }
+
     @GetMapping("/get_all_news")
     public String getAllNews(Model model) {
-        List<News> newsList=newsService.getAllNews();
-        model.addAttribute("listNews",newsList);
+        List<News> newsList = newsService.getAllNews();
+        model.addAttribute("listNews", newsList);
 //        lay ra 3 bai viet moi nhat
-        List<News> top3News=newsService.getTop3LatestNews();
-        model.addAttribute("top3LatestNews",top3News);
+        List<News> top3News = newsService.getTop3LatestNews();
+        model.addAttribute("top3LatestNews", top3News);
 //        lay danh sach cac tag
-        List<TagForNews> tagList=tagForNewsService.getAllTagsForNews();
-        model.addAttribute("listTag",tagList);
+        List<TagForNews> tagList = tagForNewsService.getAllTagsForNews();
+        model.addAttribute("listTag", tagList);
         return "customer/newsList";
     }
+
     @GetMapping("/viewNewsDetail")
     public String getNewsById(@RequestParam("newsId") int newsId, Model model) {
-        News news=newsService.getNewsById(newsId);
-        model.addAttribute("news",news);
+        News news = newsService.getNewsById(newsId);
+        model.addAttribute("news", news);
 //        lay ra 3 bai viet moi nhat
-        List<News> top3News=newsService.getTop3LatestNews();
-        model.addAttribute("top3LatestNews",top3News);
+        List<News> top3News = newsService.getTop3LatestNews();
+        model.addAttribute("top3LatestNews", top3News);
 //        lay danh sach cac tag
-        List<TagForNews> tagList=tagForNewsService.getAllTagsForNews();
-        model.addAttribute("listTag",tagList);
+        List<TagForNews> tagList = tagForNewsService.getAllTagsForNews();
+        model.addAttribute("listTag", tagList);
         return "customer/newsDetail";
     }
+
     @GetMapping("/viewAssetDetail")
     public String getAssetById(@RequestParam("assetId") int assetId, Model model) {
-        Asset asset=assetService.getAssetById(assetId);
+        Asset asset = assetService.getAssetById(assetId);
         String embedUrl = GetSrcInGoogleMapEmbededURLUtil.extractSrcFromIframe(asset.getCoordinatesOnMap());
         model.addAttribute("embedUrl", embedUrl);
-        model.addAttribute("asset",asset);
+        model.addAttribute("asset", asset);
         return "customer/assetDetail";
     }
+
     @GetMapping("/viewAuctionDetail")
     public String getAuctionById(@RequestParam("auctionId") int auctionId, Model model) {
-        AuctionSession auction=auctionService.getAuctionSessionById(auctionId);
+        AuctionSession auction = auctionService.getAuctionSessionById(auctionId);
         String embedUrl = GetSrcInGoogleMapEmbededURLUtil.extractSrcFromIframe(auction.getAsset().getCoordinatesOnMap());
         model.addAttribute("embedUrl", embedUrl);
-        model.addAttribute("auction",auction);
+        model.addAttribute("auction", auction);
         return "customer/auctionDetail";
     }
 
@@ -133,7 +134,6 @@ public class CustomerController {
             @ModelAttribute("customerDTO") CustomerDTO customerDTO,
             @RequestParam("idCardFrontImage") MultipartFile idCardFrontImage,
             @RequestParam("idCardBackImage") MultipartFile idCardBackImage, Model model) {
-
         // Extract customer and account from DTO
         Customer customer = customerDTO.getCustomer();
         Account account = customerDTO.getAccount();
@@ -153,8 +153,32 @@ public class CustomerController {
             model.addAttribute("errorMessage", "Account or Customer data is missing.");
             return "/customer/profile";
         }
-
         return "redirect:/customer/profile";
+    }
+
+    @PostMapping("/registerAuction")
+    public String registerAuction(@RequestParam("validate") String validate,
+                                  @RequestParam("auctionId") int auctionId, Model model) {
+
+        AuctionSession auction = auctionService.getAuctionSessionById(auctionId);
+        String embedUrl = GetSrcInGoogleMapEmbededURLUtil.extractSrcFromIframe(auction.getAsset().getCoordinatesOnMap());
+        model.addAttribute("embedUrl", embedUrl);
+        model.addAttribute("auction", auction);
+        //check xem nguoi dung da validate tai khoan chua
+        //kiem tra xem nguoi dung da tick het chua
+        if (validate != null) {
+            //tao ma QR chuyen tien coc
+            qrCode.setAmount(auction.getDeposit() + auction.getRegisterFee() + "");
+            qrCode.setDescription("User id 10 " + "transfer deposit, fee");
+            model.addAttribute("qrCode", qrCode);
+        }
+        return "customer/auctionDetail";
+    }
+
+    @PostMapping("/transferDepositAndFee")
+    public String transferDepositAndFee() {
+
+        return "customer/auctionDetail";
     }
 
 }
--- src/main/java/com/se1858/group4/Land_Auction_SWP391/utility/QrCode.java
+++ src/main/java/com/se1858/group4/Land_Auction_SWP391/utility/QrCode.java
@@ -1,7 +1,8 @@
 package com.se1858.group4.Land_Auction_SWP391.utility;
 
-import java.math.BigDecimal;
+import org.springframework.stereotype.Component;
 
+@Component
 public class QrCode {
     private String QrUrl;
     private final String bankId="vietinbank";
@@ -19,6 +20,42 @@ public class QrCode {
     public QrCode(String amount, String description) {
         this.amount = amount;
         this.description = convertToUrlFormat(description);
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setQrUrl(String qrUrl) {
+        QrUrl = qrUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBankId() {
+        return bankId;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getAcoountName() {
+        return acoountName;
     }
 
     public String convertToUrlFormat(String input) {
