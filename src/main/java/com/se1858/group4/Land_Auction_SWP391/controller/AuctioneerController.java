2b32438bddb452c84df58c3110bb081aeafee119
Bui Minh Chien <chien181004@gmail.com>
2024-11-05 11:10:01 +0700

    sua lat vat

--- src/main/java/com/se1858/group4/Land_Auction_SWP391/controller/AuctioneerController.java
+++ src/main/java/com/se1858/group4/Land_Auction_SWP391/controller/AuctioneerController.java
@@ -57,13 +57,8 @@ public class AuctioneerController {
         return "auctioneer/Dashboard";
     }
 
-    @GetMapping("/get_auction_list")
-    public String getAuctionList(Model model) {
-        Account auctioneer = userDetailsService.accountAuthenticated();
-        List<AuctionSession> list = auctionService.getAllAuctionSessionsByAuctioneerId(auctioneer.getAccountId());
-        model.addAttribute("listAuction", list);
-        return "auctioneer/AuctionList";
-    }
+    //@GetMapping("/get_auction_list")
+    //AuctionList.html trong auctioneer
 
     @GetMapping("/awaiting_list")
     public String getAssetAwaitingSchedulingList(Model model) {
